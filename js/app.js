f655eea6a4fe1867833debf4f258ad7f717088ce
Emilse Arias <emilsearias@gmail.com>
2025-07-24 20:11:06 -0300

    agregamos el evento submit al formulario de contacto

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 // elementos del DOM
 const btnAgregarContacto = document.getElementById('btnAgregarContacto');
 const modalFormularioContacto = new bootstrap.Modal(document.getElementById('contactoModal'))
-console.log(btnAgregarContacto)
+const formularioContacto = document.getElementById('formContacto')
+
+//Funciones
+const crearContacto = ()=>{
+    console.log('aqui tengo que crear el contacto')
+}
+
 
 //manejadores de eventos
 btnAgregarContacto.addEventListener('click', ()=>{
     modalFormularioContacto.show()
+})
+
+formularioContacto.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    //aqui tengo que crear/editar un contacto
+    crearContacto()
 })
