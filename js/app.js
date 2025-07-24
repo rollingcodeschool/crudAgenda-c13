b789d12b501b97451ddd29a5df098f9e16be1315
Emilse Arias <emilsearias@gmail.com>
2025-07-24 20:34:32 -0300

    podemos crear un objeto Contacto

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
+import Contacto from "./contacto.js";
 // elementos del DOM
 const btnAgregarContacto = document.getElementById('btnAgregarContacto');
 const modalFormularioContacto = new bootstrap.Modal(document.getElementById('contactoModal'))
 const formularioContacto = document.getElementById('formContacto')
+const inputNombre = document.getElementById('nombre');
+const inputApellido = document.getElementById('apellido');
+const inputEmail = document.getElementById('email');
+const inputTelefono = document.getElementById('telefono');
+const inputDireccion = document.getElementById('direccion');
+const inputNotas = document.getElementById('notas');
+const inputImagen = document.getElementById('imagen');
+const inputPuestoTrabajo = document.getElementById('puestoTrabajo');
+const inputEmpresa= document.getElementById('empresa');
 
 //Funciones
 const crearContacto = ()=>{
     console.log('aqui tengo que crear el contacto')
+    // todo Agregar validaciones
+    //buscar los datos del formulario y crear un objeto contacto
+    //guardar el contacto en la agenda de contactos
+    //guardar la agenda en el localstorage
+    const contactoNuevo = new Contacto(1,1,1,1,1,1,1,1,1,1)
+    console.log(contactoNuevo)
 }
 
 
