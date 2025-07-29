e1e8a52378f0cdd43b4cec267406b95861f5d701
Emilse Arias <emilsearias@gmail.com>
2025-07-29 20:17:33 -0300

    agregamos sweetalert para mostrar un mensaje cuando el contacto fue creado

--- js/app.js
+++ js/app.js
@@ -1,53 +1,69 @@
 import Contacto from "./contacto.js";
 // elementos del DOM
-const btnAgregarContacto = document.getElementById('btnAgregarContacto');
-const modalFormularioContacto = new bootstrap.Modal(document.getElementById('contactoModal'))
-const formularioContacto = document.getElementById('formContacto')
-const inputNombre = document.getElementById('nombre');
-const inputApellido = document.getElementById('apellido');
-const inputEmail = document.getElementById('email');
-const inputTelefono = document.getElementById('telefono');
-const inputDireccion = document.getElementById('direccion');
-const inputNotas = document.getElementById('notas');
-const inputImagen = document.getElementById('imagen');
-const inputPuestoTrabajo = document.getElementById('puestoTrabajo');
-const inputEmpresa= document.getElementById('empresa');
+const btnAgregarContacto = document.getElementById("btnAgregarContacto");
+const modalFormularioContacto = new bootstrap.Modal(
+  document.getElementById("contactoModal")
+);
+const formularioContacto = document.getElementById("formContacto");
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const inputTelefono = document.getElementById("telefono");
+const inputDireccion = document.getElementById("direccion");
+const inputNotas = document.getElementById("notas");
+const inputImagen = document.getElementById("imagen");
+const inputPuestoTrabajo = document.getElementById("puestoTrabajo");
+const inputEmpresa = document.getElementById("empresa");
 // verificar si el localstorage tiene contactos, si no tiene hago un array vacio
-const agenda = JSON.parse(localStorage.getItem('agendaKey')) || [];
-console.log(agenda)
+const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
+console.log(agenda);
 //Funciones
-const guardarLocalstorage = ()=>{
- localStorage.setItem('agendaKey', JSON.stringify(agenda))
+const guardarLocalstorage = () => {
+  localStorage.setItem("agendaKey", JSON.stringify(agenda));
+};
+
+const crearContacto = () => {
+  console.log("aqui tengo que crear el contacto");
+  // todo Agregar validaciones
+  //buscar los datos del formulario y crear un objeto contacto
+  const contactoNuevo = new Contacto(
+    inputNombre.value,
+    inputApellido.value,
+    inputTelefono.value,
+    inputEmail.value,
+    inputImagen.value,
+    inputEmpresa.value,
+    inputPuestoTrabajo.value,
+    inputDireccion.value,
+    inputNotas.value
+  );
+  //guardar el contacto en la agenda de contactos
+  agenda.push(contactoNuevo);
+  console.log(contactoNuevo);
+  //guardar la agenda en el localstorage
+  guardarLocalstorage();
+  //mostrar un mensaje al usuario final
+  Swal.fire({
+    title: "Contacto creado",
+    text: `El contacto ${inputNombre.value} fue creado correctamente.`,
+    icon: "success",
+    confirmButtonText: "Ok",
+  });
+  //limpiar el formulario
+  limpiarFormulario();
+};
+
+function limpiarFormulario() {
+  formularioContacto.reset();
 }
-
-const crearContacto = ()=>{
-    console.log('aqui tengo que crear el contacto')
-    // todo Agregar validaciones
-    //buscar los datos del formulario y crear un objeto contacto
-    const contactoNuevo = new Contacto(inputNombre.value, inputApellido.value, inputTelefono.value, inputEmail.value, inputImagen.value, inputEmpresa.value, inputPuestoTrabajo.value, inputDireccion.value, inputNotas.value)
-    //guardar el contacto en la agenda de contactos
-    agenda.push(contactoNuevo)
-    console.log(contactoNuevo)
-    //guardar la agenda en el localstorage
-    guardarLocalstorage();
-    //mostrar un mensaje al usuario final
-    //limpiar el formulario
-    limpiarFormulario()
-}
-
-function limpiarFormulario(){
-    formularioContacto.reset()
-}
-
-
 
 //manejadores de eventos
-btnAgregarContacto.addEventListener('click', ()=>{
-    modalFormularioContacto.show()
-})
+btnAgregarContacto.addEventListener("click", () => {
+  modalFormularioContacto.show();
+});
 
-formularioContacto.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    //aqui tengo que crear/editar un contacto
-    crearContacto()
-})
+formularioContacto.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //aqui tengo que crear/editar un contacto
+  crearContacto();
+});
